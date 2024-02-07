e4f5dd4ee62edee2d4de3b4b5fb492591ad14f51
Ilia-Gharib <159142167+Ilia-Gharib@users.noreply.github.com>
2024-02-07 14:02:54 +0330

    first file added

---
+++ 01/01.js
@@ -0,0 +1,18 @@
+console.log("hello world!");
+let a=1;
+let b=2;
+console.log("sum a and b is",a+b);
+let x=1;
+console.log("type of x is",typeof x,"and value of x is",x);
+x="one";
+console.log("type of x is",typeof x,"and value of x is",x);
+x=[1,2,"three","four"];
+console.log("type of x is",typeof x,"and value of x is",x);
+console.leg("number 0 of x is",x[0]);
+x={
+    myKey:111,
+    myotherKey: "two"
+};
+console.log("type of x is",typeof x,"and value of x is",x);
+console.leg("x [myKey] is",x["myKey"]);
+console.leg("x [myKey] is",x.myKey);
